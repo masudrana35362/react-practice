a1dfa52356c5e305940562d60b549fd8e8ee5612
Md Masud Rana <masudrana35362@gmail.com>
2024-01-21 16:01:22 +0600

    working class component

--- App.js
+++ App.js
@@ -5,79 +5,24 @@
  * @format
  */
 
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import {
-  Button,
   FlatList,
-  Image,
-  ScrollView,
-  StyleSheet,
   Text,
-  TextInput,
   View,
 } from 'react-native';
 
-import UserData from './components/UserData';
+class App extends Component{
 
+  render(){
+    return(
+      <View>
+<Text style= {{fontSize: 30,color: 'red'}}> Class Component </Text>
 
-const App = () => {
+      </View>
+    )
+  }
 
-  const users = [
-    {
-      id: 1,
-      name: "Masud",
-      email: "example@gmail.com"
-    },
-    {
-      id: 2,
-      name: "Rana",
-      email: "example@gmail.com"
-    },
-    {
-      id: 3,
-      name: "Milon",
-      email: "example@gmail.com"
-    },
-    {
-      id: 4,
-      name: "Sajib",
-      email: "example@gmail.com"
-
-    },
-    {
-      id: 5,
-      name: "Masud",
-      email: "example@gmail.com"
-    },
-    {
-      id: 6,
-      name: "Rana",
-      email: "example@gmail.com"
-    },
-    {
-      id: 7,
-      name: "Milon",
-      email: "example@gmail.com"
-    },
-    {
-      id: 8,
-      name: "Sajib",
-      email: "example@gmail.com"
-    },
-  ]
-
-  return (
-    <View>
-      <Text style={{ fontSize: 24 }} >Name List showing here </Text>
-
-      <FlatList style={{ marginBottom: 50 }}
-        data={users}
-        renderItem={({ item }) => <UserData item={item}/>}
-        keyExtractor={item => item.id}
-      />
-    </View>
-
-  );
-};
+}
 
 export default App;
