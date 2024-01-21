80ecbfffecbc7d03e81d1721860ec0891de3f645
Md Masud Rana <masudrana35362@gmail.com>
2024-01-21 12:38:16 +0600

    list using map

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   Button,
   FlatList,
   Image,
+  ScrollView,
   StyleSheet,
   Text,
   TextInput,
@@ -36,18 +37,40 @@ const App = () => {
       id: 4,
       name: "Sajib"
     },
-
+    {
+      id: 5,
+      name: "Masud"
+    },
+    {
+      id: 6,
+      name: "Rana"
+    },
+    {
+      id: 7,
+      name: "Milon"
+    },
+    {
+      id: 8,
+      name: "Sajib"
+    },
   ]
 
   return (
     <View>
       <Text style={{ fontSize: 24 }} >Name List showing here </Text>
 
-      <FlatList 
-      data={users}
-      renderItem={({item})=> <Text style= {styles.textStyle} >{item.name}</Text>}
-      keyExtractor={item=> item.id}
-      />
+      {/* <FlatList
+        data={users}
+        renderItem={({ item }) => <Text style={styles.textStyle} >{item.name}</Text>}
+        keyExtractor={item => item.id}
+      /> */}
+      <ScrollView style={{marginBottom:50}}>
+        {
+          users.map((item) => <Text style={styles.textStyle} >{item.name}</Text>)
+        }
+      </ScrollView>
+
+
 
     </View>
 
@@ -56,8 +79,9 @@ const App = () => {
 
 const styles = StyleSheet.create({
   textStyle: {
+    height: 100,
     color: '#fff',
-    fontSize: 16,
+    fontSize: 22,
     backgroundColor: 'green',
     marginTop: 10,
     marginHorizontal: 10,
