a168e95ddb15e429c147f2be36a2d74da46a33af
Md Masud Rana <masudrana35362@gmail.com>
2024-01-22 12:33:32 +0600

    showing modal

--- App.js
+++ App.js
@@ -5,24 +5,71 @@
  * @format
  */
 
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import {
-  FlatList,
+  Button,
+  Modal,
+  StyleSheet,
   Text,
   View,
 } from 'react-native';
 
-class App extends Component{
+const App = () => {
 
-  render(){
-    return(
-      <View>
-<Text style= {{fontSize: 30,color: 'red'}}> Class Component </Text>
+  const [showModal,setShowModal] = useState(false)
 
+  return (
+    <View style={styles.main}>
+
+      <Modal
+       transparent={true} 
+      visible={showModal}
+      animationType='fade'
+
+      >
+
+        <View style={styles.centerView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}> this is modal</Text>
+            <Button title='Cloase Modal' onPress={()=> setShowModal(false)}/>
+          </View>
+        </View>
+
+
+      </Modal>
+
+      <View style={styles.buttonView}>
+        <Button title='Open Modal' onPress={()=>setShowModal(true)} />
       </View>
-    )
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1
+  },
+  buttonView: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  centerView: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  modalView: {
+    backgroundColor: 'white',
+    padding:40,
+    borderRadius:20,
+    shadowColor: 'black',
+    elevation: 5,
+  },
+  modalText:{
+    marginBottom: 10,
+    fontSize:20
   }
 
-}
+})
 
 export default App;
